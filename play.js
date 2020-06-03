427a15fc552bb666410f6df299b60ae7f9218905
Jaycob Duffel <jbdcoding@gmail.com>
2020-06-03 21:34:04 +0000

    can exit program play.js

--- play.js
+++ play.js
@@ -6,3 +6,25 @@ const connect = require('./client')
 
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    handleUserInput(key)
+  })
+  return stdin;
+}
+
+const handleUserInput = (key) => {
+if (key === '\u0003') {
+  process.exit();
+}
+}
+// '\u0003'
+
+setupInput();
+  
+  
